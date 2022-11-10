1616bfa36fcebb226d3a7ef92cfeaf15c50dc639
Maria Julia Valaski <maju.m.valaski@gmail.com>
2022-11-10 15:59:29 -0200

    Writing JS to get the dropdown menus

---
+++ assets/index.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the features content */
+function myFunction1() {
+    document.getElementById("myDropdownF").classList.toggle("showF");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  // Features button
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+      var features = document.getElementsByClassName("features-content");
+      var i;
+      for (i = 0; i < features.length; i++) {
+        var openFeatures = features[i];
+        if (openFeatures.classList.contains('showF')) {
+          openFeatures.classList.remove('showF');
+        }
+      }
+    }
+  }
+
+  function myFunction2() {
+    document.getElementById("myDropdownC").classList.toggle("showC");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  // Features button
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2')) {
+      var company = document.getElementsByClassName("company-content");
+      var i;
+      for (i = 0; i < company.length; i++) {
+        var openCompany = company[i];
+        if (openCompany.classList.contains('showC')) {
+          openCompany.classList.remove('showC');
+        }
+      }
+    }
+  }
